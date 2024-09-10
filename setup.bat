a146e38971f6b075b44e4a6c00c9caf1151a007b
Paloox <167614497+Paloox@users.noreply.github.com>
2024-09-11 00:17:55 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,8 @@
+python -m pip install opencv-python
+python -m pip install numpy
+python -m pip install mss
+python -m pip install pywin32
+python -m pip install pyserial
+python -m pip install pyautogui
+
+pause
